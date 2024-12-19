970e53c38d6f76cca6f33cdd535ef462490e3ef0
VishLella <vishlella.work@gmail.com>
2024-12-19 13:06:29 -0500

    fixed ReadHeaderTimeout

--- json.go
+++ json.go
@@ -29,9 +29,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
 	if err != nil {
-		log.Printf("Error writing JSON: %s", err)
-		w.WriteHeader(500)
-		return
+		log.Printf("Critical error writing response: %s", err)
 	}
 
 }
